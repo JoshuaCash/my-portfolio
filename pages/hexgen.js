520ae906e70e02f51ceb5ead4fa6ae36ab3b81de
Josh Cash <joshcash86@gmail.com>
2022-11-04 12:04:37 -0400

    add portfolio files

---
+++ pages/hexgen.js
@@ -0,0 +1,20 @@
+const hex = document.querySelector(".counter-hex");
+const btn = document.querySelector(".generate")
+const title = document.querySelector(".counter-title");
+const container = document.querySelector(".counter-container")
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8).toUpperCase();
+    document.body.style.backgroundColor = "#" + randomColor;
+    container.style.color = "#" + randomColor;
+    btn.style.backgroundColor = "#" + randomColor;
+    hex.textContent = "#" + randomColor;
+}
+
+
+
+
+btn.addEventListener("click", generateColor);
+
+
+generateColor();
